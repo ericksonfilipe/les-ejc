814816e19b4d35db559e78acd49575ee4f9b8822
andreh aranha pb <andreh.aranha.pb@gmail.com>
2011-03-03 19:39:48 -0300

    generate-all de Usuario adicionado.

--- grails-app/domain/les/ejc/Usuario.groovy
+++ grails-app/domain/les/ejc/Usuario.groovy
@@ -2,6 +2,11 @@ package les.ejc
 
 class Usuario {
 
+    String nome
+    String sobrenome
+    
     static constraints = {
+        nome(nullable:false, blank:false, size:2..20)
+        sobrenome(nullable:false, blank:false, size:2..100)
     }
 }
