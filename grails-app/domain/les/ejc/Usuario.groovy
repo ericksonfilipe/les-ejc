4092bd43a2ffca93636ebfc56a68ef0fb12dded7
andreh aranha pb <andreh.aranha.pb@gmail.com>
2011-03-03 19:54:48 -0300

    Atualizando Usuario

--- grails-app/domain/les/ejc/Usuario.groovy
+++ grails-app/domain/les/ejc/Usuario.groovy
@@ -6,7 +6,7 @@ class Usuario {
     String sobrenome
     
     static constraints = {
-        nome(nullable:false, blank:false, size:2..20)
-        sobrenome(nullable:false, blank:false, size:2..100)
+        nome(nullable:false, blank:false, size:2..20, matches:'[a-zA-Z]+')
+        sobrenome(nullable:false, blank:false, size:2..100, matches:'([a-zA-Z]| )+')
     }
 }
