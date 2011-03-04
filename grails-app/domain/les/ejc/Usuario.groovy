dd628f57b53fc80c26ef55783f70fdbbcc9bfc6a
andreh aranha pb <andreh.aranha.pb@gmail.com>
2011-03-04 00:29:22 -0300

    Criando classe dominio Usuario

--- grails-app/domain/les/ejc/Usuario.groovy
+++ grails-app/domain/les/ejc/Usuario.groovy
@@ -2,6 +2,19 @@ package les.ejc
 
 class Usuario {
 
+    String nome
+    String sobrenome
+    String telefone
+    String email
+    byte[] foto
+
+
+    //Endereco end
+    //Paroquia paroquia
+    //List<Equipes> equipesTrabalhadas
+    //Enum Status
+    //List<String> Observacoes
+
     static constraints = {
     }
 }
