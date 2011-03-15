330df87ab695c2f07bd17f52f2c6dfb4b9c93ea1
ericksonfilipe <ericksonfilipe@gmail.com>
2011-03-15 00:11:34 -0300

    fixed Aniversario job, solu??o encontrada: remover o plugin de teste funcional

--- grails-app/jobs/les/ejc/AniversarioJob.groovy
+++ grails-app/jobs/les/ejc/AniversarioJob.groovy
@@ -2,7 +2,7 @@ package les.ejc
 
 class AniversarioJob {
      static triggers = {
-		cron name: 'aniversarioTrigger', cronExpression: "0 49 10 * * ?"
+		cron name: 'aniversarioTrigger', cronExpression: "0 5 0 * * ?"
 	}
 
 	def senderService
@@ -10,16 +10,17 @@ class AniversarioJob {
     def execute() {
 		def usuarios = Usuario.list()
 		Date data = new Date()
+		println("${data.day}")
 		for (int i in 0..<usuarios.size()) {
 			if ( usuarios[i]?.dataDeNascimento?.day == data.day && usuarios[i]?.dataDeNascimento?.month == data.month ) {
-				String mensagem = """Parabens, ${usuarios[i].nome}!
+				String mensagem = """Parabens, ${usuarios[i].nomeCompleto}!
 O EJC da Paroquia de Sao Cristovao o(a) felicita por mais um ano de vida.
 Deus o(a) abencoe cada vez mais.
 Abracos!
 """
 				println("enviar email diario de aniversario...")
 				senderService.enviaEmail(usuarios[i].email, "Feliz Aniversario", mensagem)
-				println("envio para ${usuarios[i].nome} ok!")
+				println("envio para ${usuarios[i].nomeUsual} ok!")
 			}
 		}
     }
