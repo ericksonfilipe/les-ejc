d7a0d19e6ed20366c65a56989b80a1a5745e80fc
Andr? <andreh.aranha.pb@gmail.com>
2011-04-11 00:26:25 -0300

    Bug 4 morto

--- grails-app/domain/les/ejc/Encontro.groovy
+++ grails-app/domain/les/ejc/Encontro.groovy
@@ -12,8 +12,8 @@ class Encontro {
     static hasMany = [circulos:Circulo, equipesDeTrabalho:EquipeDeTrabalho]
 
     static constraints = {
-        dirigenteEspiritual(nullable:false, validator: {if (it.tipo == Tipo.Padre) return true
-                                                        else return ["erro.tipo.invalido", it.tipo]})
+        dirigenteEspiritual(nullable:false, validator: {if (it?.tipo == Tipo.Padre) return true
+                                                        else return ["erro.tipo.invalido", it?.tipo]})
         data(nullable:false)
         tema(nullable:false, blank:false, matches:'([a-zA-Z]| )+')
         local(nullable:false, blank:false)
