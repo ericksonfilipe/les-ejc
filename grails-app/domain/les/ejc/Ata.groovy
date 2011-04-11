3c1e5f965f7dcf86aafd8daf5efc79283060ca6d
Andr? <andreh.aranha.pb@gmail.com>
2011-04-10 23:04:20 -0300

    Bug 15 morto

--- grails-app/domain/les/ejc/Ata.groovy
+++ grails-app/domain/les/ejc/Ata.groovy
@@ -8,8 +8,16 @@ class Ata {
 
     static constraints = {
 		data()
+		
+//		dirigenteEspiritual(nullable:false, validator: {if (it.tipo == Tipo.Padre) return true else return ["erro.tipo.invalido", it.tipo]})
+//		 senha2(validator:  {valor, objeto -> if (objeto.properties['senha2'] == valor) return true else return "erro.senhas.diferentes" })
 		pessoasPresentes(nullable:false, minSize:2)
-		pessoasAusentes()
+		pessoasAusentes(validator: {valor, objeto -> 
+		for (Usuario u : valor)
+		    if (objeto.properties['pessoasPresentes'].contains(u)) return  "erro.valor.invalido";
+        return true })
+		
+//		pessoasAusentes()
 		pauta(nullable:false, blank:false, maxSize:2000)
     }
 
