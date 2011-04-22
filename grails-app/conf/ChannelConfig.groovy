d7dbaa20ac26a22363f29532cf2582af031aaf14
Leandro de Assis <leandrodiassis@gmail.com>
2011-04-22 17:56:59 -0300

    Desativando HTTPS

--- grails-app/conf/ChannelConfig.groovy
+++ grails-app/conf/ChannelConfig.groovy
@@ -20,10 +20,10 @@ class ChannelConfig {
 		requestMap.put new RequestKey("/auth/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
 		requestMap.put new RequestKey("/admin/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
 		requestMap.put new RequestKey("/app/account/edituser"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
-		requestMap.put new RequestKey("/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
+		//requestMap.put new RequestKey("/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
 
 		// resources that must be served over http (basically everything else not already listed above)
-		//requestMap.put new RequestKey("/**"), [new SecurityConfig("REQUIRES_INSECURE_CHANNEL")] // all other pages should be served over http
+		requestMap.put new RequestKey("/**"), [new SecurityConfig("REQUIRES_INSECURE_CHANNEL")] // all other pages should be served over http
 
 		requestMap
 	}
