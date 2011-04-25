3fa997e3f516ae59210e2835b1083c508359bc18
andreh aranha pb <andreh.aranha.pb@gmail.com>
2011-04-24 21:47:36 -0300

    Senha aleatroia de 8 digitos

---
+++ grails-app/domain/les/ejc/Usuario.groovy
@@ -0,0 +1,154 @@
+package les.ejc
+
+import java.util.Random
+
+class Usuario {
+
+    String nomeCompleto
+    String nomeUsual
+    Calendar dataDeNascimento
+	
+    Telefone telefone1
+    Telefone telefone2
+    Telefone telefone3
+    
+    String email
+    byte[] foto
+	byte[] ficha
+    String paroquia
+    String equipesTrabalhadas
+    String observacoes
+    Status status
+    Tipo tipo
+    boolean j5Atual
+	
+    private String login
+    private String senha
+
+	static hasMany = [atas:Ata]
+	static belongsTo = [Ata]
+	static hasOne = [ endereco : Endereco ]
+
+	
+    static constraints = {
+        nomeCompleto(blank:false, size:2..100, matches:'([a-zA-Z]| )+')
+        nomeUsual(blank:true, size:2..40, matches:'([a-zA-Z]| )+')
+        dataDeNascimento(nullable:true)
+        endereco(nullable:true)
+		telefone1(nullable:true)
+		telefone3(nullable:true)
+		telefone2(nullable:true)
+		//telefone1(blank:true, maxSize:13, minSize:13, matches:'.[0-9][0-9].[0-9][0-9][0-9][0-9].[0-9][0-9][0-9][0-9]')
+        //telefone2(blank:true, maxSize:13, minSize:13, matches:'.[0-9][0-9].[0-9][0-9][0-9][0-9].[0-9][0-9][0-9][0-9]')
+        //telefone3(blank:true, maxSize:13, minSize:13, matches:'.[0-9][0-9].[0-9][0-9][0-9][0-9].[0-9][0-9][0-9][0-9]')
+        email(nullable:true, email:true, unique:true)
+		foto(nullable:true)
+		ficha(nullable:true)
+        paroquia(blank:true, matches:'([a-zA-Z]| )+')
+        equipesTrabalhadas(blank:true, matches:'([a-zA-Z]| )+')
+        observacoes(blank:true, matches:'([a-zA-Z]| )+')
+		senha(nullable:true, password:true)
+		login(nullable:true, unique:true)
+		j5Atual()
+    }
+	
+    enum Status {
+	Ativo, Casado, Impedido, Sem_Contato
+    }
+
+    enum Tipo {
+	Jovem, Casal, Padre
+    }
+	
+	public String getLogin(){
+		return login
+	}
+	
+	public setLogin(String novoLogin) {
+		login = novoLogin
+	}
+	
+	public String getSenha(){
+		return senha
+	}
+	
+	public setSenha(String novaSenha) {
+		senha = novaSenha
+	}
+	
+	/**
+	Altera a senha fazendo testes de senha segura - metodo a ser modificado dependendo do cliente
+	*/
+	public alteraSenha(String novaSenha) {
+		senha=novaSenha
+	}
+	
+	/**
+	Uma vez que um usuario eh criado, testa se ele terah alguns preenchimentos automaticos
+	*/
+	public realizaCriacoesAutomaticas() {
+		geraNomeUsualAutomatico()
+		geraLoginSenhaAutomaticos()
+	}
+	
+	private String getSenhaAleatoria(int digitos) {
+		String senha = "";
+		(1..digitos).each {  
+			senha += getRandomChar()
+		}
+		
+		return senha
+	}
+	
+	private String getRandomChar() {
+		def randomIntegerList = []
+		
+		randomIntegerList << getRandomNumber()
+		randomIntegerList << getRandomCharMaiusculo()
+		randomIntegerList << getRandomCharMinusculo()
+		
+		Random random = new Random()
+		return randomIntegerList[random.nextInt(3)]
+	}
+	
+	private String getRandomNumber() {
+		Random random = new Random()
+		return (char) (48 + random.nextInt(10))
+	}
+	
+	private String getRandomCharMaiusculo() {
+		Random random = new Random()
+		return (char) (65 + random.nextInt(26))
+	}
+	
+	private String getRandomCharMinusculo() {
+		Random random = new Random()
+		return (char) (97 + random.nextInt(26))
+	}
+	
+	/**
+	Se usuario tem nomeUsual em branco, deve ser seu primeiro nome
+	*/
+	private geraNomeUsualAutomatico() {
+		if (nomeUsual.equals("")) {
+			nomeUsual = nomeCompleto.split(" ")[0]
+		}
+	}
+
+	/**
+	Se usuario tem email e eh cadastrado Ativo, deve ter login e senha
+	*/
+	private geraLoginSenhaAutomaticos() {
+        if (status == Status.Ativo && (email != null)) {
+			login = email
+			//def novaSenha = nomeUsual
+			//if (dataDeNascimento != null) { novaSenha += dataDeNascimento.get(Calendar.YEAR) }
+			senha = getSenhaAleatoria(8)
+		}
+	}
+
+	String toString() {
+		return "${nomeCompleto}";
+	}
+		
+}
---
+++ test/functional/les/ejc/UsuarioMembroEJCFunctionalTests.groovy
@@ -0,0 +1,127 @@
+package les.ejc
+
+class UsuarioMembroEJCFunctionalTests extends functionaltestplugin.FunctionalTestCase {
+  
+	private String defaultLocation =  "http://localhost:8080/les-ejc/"
+	
+	void testTituloBasico(){
+		get(this.defaultLocation);
+		form() {
+			login = "andre"
+			senha = "andre"
+			click "Login"
+		}
+		
+		get("http://localhost:8080/les-ejc/usuario/index");
+		assertTitle("Lista de Usuario")
+		assertContentContains("Lista de Usuario")
+	}
+	
+	void testCriarUsuario() {
+		get(this.defaultLocation);
+		form() {
+			login = "andre"
+			senha = "andre"
+			click "Login"
+		}
+		
+		get("http://localhost:8080/les-ejc/usuario/create");
+		assertTitle("Permissao Negada")
+		assertContentContains("Voce nao tem permissao para visualizar essa pagina.")
+	}
+	
+	void testVisualizarUsuario() {
+		get(this.defaultLocation);
+		form() {
+			login = "andre"
+			senha = "andre"
+			click "Login"
+		}
+		
+		get("http://localhost:8080/les-ejc/usuario/index");
+		click "1"
+		assertTitle("Mostrar Usuario")
+		assertContentContains("admin@plecas.com")
+	}
+	
+	void testEditarUsuarioDiferente() {
+		get(this.defaultLocation);
+		form() {
+			login = "andre"
+			senha = "andre"
+			click "Login"
+		}
+		
+		get("http://localhost:8080/les-ejc/usuario/index");
+		click "1"
+		assertTitle("Mostrar Usuario")
+		assertContentContains("admin@plecas.com")
+		form() {
+			click "Editar"
+		}
+		assertTitle("Permissao Negada")
+		assertContentContains("Voce nao tem permissao para visualizar essa pagina.")
+	}
+	
+	void testEditarProprioUsuario() {
+		get(this.defaultLocation);
+		form() {
+			login = "andre"
+			senha = "andre"
+			click "Login"
+		}
+		
+		get("http://localhost:8080/les-ejc/usuario/index");
+		click "2"
+		assertTitle("Mostrar Usuario")
+		assertContentContains("andre@plecas.com")
+		form() {
+			click "Editar"
+		}
+		assertTitle("Edit Usuario")
+		assertContentContains("Edit Usuario")
+		form() {
+			nomeCompleto = "Andre Farias"
+			click "Alterar"
+		}
+		assertContentContains "alterado"
+	}
+	
+	void testExcluirUsuarioDiferente() {
+		get(this.defaultLocation);
+		form() {
+			login = "andre"
+			senha = "andre"
+			click "Login"
+		}
+		
+		get("http://localhost:8080/les-ejc/usuario/index");
+		click "1"
+		assertTitle("Mostrar Usuario")
+		assertContentContains("admin@plecas.com")
+		form() {
+			click "Excluir"
+		}
+		assertTitle("Permissao Negada")
+		assertContentContains("Voce nao tem permissao para visualizar essa pagina.")
+	}
+	
+	void testExcluirProprioUsuario() {
+		get(this.defaultLocation);
+		form() {
+			login = "andre"
+			senha = "andre"
+			click "Login"
+		}
+		
+		get("http://localhost:8080/les-ejc/usuario/index");
+		click "2"
+		assertTitle("Mostrar Usuario")
+		assertContentContains("andre@plecas.com")
+		form() {
+			click "Excluir"
+		}
+		assertTitle("Permissao Negada")
+		assertContentContains("Voce nao tem permissao para visualizar essa pagina.")
+	}
+}
