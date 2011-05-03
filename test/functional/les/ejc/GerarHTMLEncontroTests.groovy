df99070342a7394d0d964878290757d6cc78baf9
Leandro de Assis <leandrodiassis@gmail.com>
2011-05-03 16:03:22 -0300

    Fixado tamanho de Encontro.tema e Encontro.local (size:3..50), Teste de Gerar HTML passando, e Retirado o nomeEquipe da Gerador HTML, j? que n?o pode modificar o nome.

--- test/functional/les/ejc/GerarHTMLEncontroTests.groovy
+++ test/functional/les/ejc/GerarHTMLEncontroTests.groovy
@@ -2,33 +2,23 @@ package les.ejc
 
 class GerarHTMLEncontroTests extends functionaltestplugin.FunctionalTestCase {
   
-	private String defaultLocation =  "localhost:8080/les-ejc/encontro/show/1"
-	
-	void testTituloBasico() {
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
-		get(this.defaultLocation)
-		assertTitle("Mostrar Encontro")
-	}
+	private String defaultLocation = "http://localhost:8080/les-ejc/encontro/show/1"
 	
 	void testHTML() {
-		get("localhost:8080/les-ejc/encontro/show/1");
+		get("http://localhost:8080/les-ejc/");
 		form() {
-			click "Imprimir"
+		    login = "admin"
+		    senha = "admin123"
+                    click "Login"
 		}
-
+		get(this.defaultLocation)
+                form() {
+                    click "Imprimir"
+                }
 		assertTitle("Impressão de Encontros")
 		assertContentContains "Encontro Paróquia São Cristóvão"
-		assertContentContains "Data: 02/05/2011"
-		assertContentContains "Dirigente Espiritual: Andre"
+		assertContentContains "Dirigente Espiritual: Andre Farias"
 		assertContentContains "Tema: Mei do Mato"
 		assertContentContains "Local: Manaus"
-		assertContentContains "Círculo(s):"
-		assertContentContains "Nome da Equipe: COORDENACAO_GERAL"
-
+	}
 }
