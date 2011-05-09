7c6661dcd6cd2bb0bd8b062e963394aa2810e178
Erickson <ericksonfilipe@gmail.com>
2011-05-08 23:30:51 -0300

    Job de anivers?rio contemplando usu?rio do tipo casal

--- grails-app/jobs/les/ejc/AniversarioJob.groovy
+++ grails-app/jobs/les/ejc/AniversarioJob.groovy
@@ -12,6 +12,7 @@ class AniversarioJob {
 		Calendar data = new GregorianCalendar()
 		for (int i in 0..<usuarios.size()) {
 			Calendar dataUsuario = usuarios[i].dataDeNascimento
+			Calendar dataUsuario2 = usuarios[i].dataDeNascimento2
 			if ( dataUsuario?.get(Calendar.DAY_OF_MONTH) == data.get(Calendar.DAY_OF_MONTH) && dataUsuario?.get(Calendar.MONTH) == data.get(Calendar.MONTH) ) {
 				String mensagem = """Parabens, ${usuarios[i].nomeCompleto}!
 O EJC da Paroquia de Sao Cristovao o(a) felicita por mais um ano de vida.
@@ -19,6 +20,14 @@ Deus o(a) abencoe cada vez mais.
 Abracos!
 """
 				senderService.enviaEmail(usuarios[i].email, "Feliz Aniversario", mensagem)
+			}
+			if ( dataUsuario2?.get(Calendar.DAY_OF_MONTH) == data.get(Calendar.DAY_OF_MONTH) && dataUsuario2?.get(Calendar.MONTH) == data.get(Calendar.MONTH) ) {
+				String mensagem = """Parabens, ${usuarios[i].nomeCompleto2}!
+O EJC da Paroquia de Sao Cristovao o(a) felicita por mais um ano de vida.
+Deus o(a) abencoe cada vez mais.
+Abracos!
+"""
+				senderService.enviaEmail(usuarios[i].email2, "Feliz Aniversario", mensagem)
 			}
 		}
     }
