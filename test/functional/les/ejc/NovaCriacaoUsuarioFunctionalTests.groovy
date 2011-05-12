322d818fb38162020091cc3e82bac39f7cf246d3
janderson jason <janderson.jason@gmail.com>
2011-05-12 01:29:25 -0300

    Testes NovaCriacaoUsuario e PerfilUsuario

--- test/functional/les/ejc/NovaCriacaoUsuarioFunctionalTests.groovy
+++ test/functional/les/ejc/NovaCriacaoUsuarioFunctionalTests.groovy
@@ -3,86 +3,24 @@ package les.ejc
 class NovaCriacaoUsuarioFunctionalTests extends functionaltestplugin.FunctionalTestCase {
   
 	private String defaultLocation =  "http://localhost:8080/les-ejc/usuario/create"
-	
 
-	
-	void testListaUsuarios(){
+	void testAdicionarEnderecoSoAposCriacaoBasicaDeUsuario(){
 		get("http://localhost:8080/les-ejc/");
 		form() {
 			login = "admin"
 			senha = "admin123"
 			click "Login"
 		}
-		
-		get("http://localhost:8080/les-ejc/usuario/list");
-		assertContentContains "Lista de Usuario"
-	}
-	
-	void testCadastroValido() {
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
 		get(this.defaultLocation);
+		//nao aparece endereco
 		form() {
-			nomeCompleto = "Jose da Silva"
+			nomeCompleto = "Nome da Silva"
 			click "Criar"
 		}
-		assertContentContains "criado"	
-	}
-
-	void testCadastroNulo(){
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
-		get(this.defaultLocation);
-		form() {
-			click "Criar"
-		}
-		assertContentContains "O campo Nome Completo deve ser preenchido!"
-	}
-
-	
-	void testCadastroNomeVazio(){
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
-		get(this.defaultLocation);
-		form() {
-			nomeCompleto = ""
-			click "Criar"
-		}
-		assertContentContains "O campo Nome Completo deve ser preenchido!"
-	}
-
-	void testCadastroNomeInvalido(){
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
-		get(this.defaultLocation);
-		form() {
-			nomeCompleto = "123456"
-			click "Criar"
-		}
-		assertContentContains "O campo [nomeCompleto] nao atende ao padrao definido"
+		assertContentContains "Adicionar Endereco"
 	}
 	
-	void testCadastroDeNomeUsualInvalido(){
+	void testEnderecoSempreFiliadoAUsuario(){
 		get("http://localhost:8080/les-ejc/");
 		form() {
 			login = "admin"
@@ -90,84 +28,19 @@ class NovaCriacaoUsuarioFunctionalTests extends functionaltestplugin.FunctionalT
 			click "Login"
 		}
 		
-		get(this.defaultLocation);
+		//criar endereco pra usuario Andre
+		get("http://localhost:8080/les-ejc/endereco/create?usuario.id=2");
 		form() {
-			nomeCompleto = "Jose da Silva"
-			nomeUsual = "Ze1"
+			rua = "rua"
+			bairro = "bairro"
+			cidade = "cg"
 			click "Criar"
 		}
-		assertContentContains "O campo [nomeUsual] nao atende ao padrao definido"
-	}
-
-	void testCadastroDeParoquiaInvalido(){
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
-		get(this.defaultLocation);
-		form() {
-			nomeCompleto = "Jose da Silva"
-			paroquia = "3 irmas"
-			click "Criar"
-		}
-		assertContentContains "O campo [paroquia] nao atende ao padrao definido [([a-zA-Z]| )+]"
-	}
-
-	void testCadastroDeEquipesTrabalhadasInvalido(){
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
-		get(this.defaultLocation);
-		form() {
-			nomeCompleto = "Jose da Silva"
-			equipesTrabalhadas = "Mais de 1000"
-			click "Criar"
-		}
-		assertContentContains "O campo [equipesTrabalhadas] nao atende ao padrao definido [([a-zA-Z]| )+]"
-	}
-
-	void testCadastroObservacoesInvalido(){
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
-		get(this.defaultLocation);
-		form() {
-			nomeCompleto = "Jose da Silva"
-			observacoes = "123"
-			click "Criar"
-		}
-		assertContentContains "O campo [observacoes] nao atende ao padrao definido [([a-zA-Z]| )+]"
-	}
-
-	void testEmailInvalido(){
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
-		}
-		
-		get(this.defaultLocation);
-		form() {
-			nomeCompleto = "Jose da Silva"
-			email = "email@qualquer"
-			click "Criar"
-		}
-		assertContentContains "O campo [email] nao e um endereco de email valido"
+		assertContentContains "criado"
+		assertContentContains "Voltar para o Usuario: Andre"
 	}
 	
-	void testNomeUsualAutomatico(){
+	void testUnicoEnderecoParaUsuario(){
 		get("http://localhost:8080/les-ejc/");
 		form() {
 			login = "admin"
@@ -175,31 +48,21 @@ class NovaCriacaoUsuarioFunctionalTests extends functionaltestplugin.FunctionalT
 			click "Login"
 		}
 		
-		get(this.defaultLocation);
+		//criar endereco pra usuario Andre
+		get("http://localhost:8080/les-ejc/endereco/create?usuario.id=2");
 		form() {
-			nomeCompleto = "Jose da Silva"
-			nomeUsual = ""
+			rua = "rua"
+			bairro = "bairro"
+			cidade = "cg"
 			click "Criar"
-		}
-		assertContentContains '<tdvalign="top"class="name">nomeusual</td><tdvalign="top"class="value">jose</td>'
-	}
-	/*
-	void testTelefoneInvalido(){
-		get("http://localhost:8080/les-ejc/");
-		form() {
-			login = "admin"
-			senha = "admin123"
-			click "Login"
 		}
 		
-		get(this.defaultLocation);
-		form() {
-			nomeCompleto = "Jose da Silva"
-			telefone = "12345"
-			click "Criar"
-		}
-		assertContentContains "O campo [telefone] com o valor [12345] nao atinge o tamanho minimo de [10]"
+		get("http://localhost:8080/les-ejc/usuario/show/2")
+		assertContentContains "Rua: rua - numero: 0. bairro - cg/PB"
+		//nao tem opcao adicionar endereco
+		
 	}
-	*/
+
+	
 
 }
