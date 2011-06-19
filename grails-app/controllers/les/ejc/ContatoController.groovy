9707ed0457b74dfd3792d9564931bc1788e70851
Erickson <ericksonfilipe@gmail.com>
2011-06-19 16:13:24 -0300

    terminando de implementar a p?gina de contato

--- grails-app/controllers/les/ejc/ContatoController.groovy
+++ grails-app/controllers/les/ejc/ContatoController.groovy
@@ -1,5 +1,7 @@
 package les.ejc
 
+import java.util.regex.Pattern;
+
 class ContatoController {
 
 	def senderService
@@ -10,8 +12,14 @@ class ContatoController {
 		if (params.mensagem == null || params.email == null || params.mensagem == "" || params.email == "") {
 			flash.message = "Você precisa preencher os campos abaixo!"
 			redirect(controller: "contato", action:"index")
+		} else if (params.mensagem.size() < 15) {
+			flash.message = "A mensagem precisa ter pelo menos 15 caracteres!"
+			redirect(controller: "contato", action:"index")
+		} else if (!params.email.matches("([a-zA-Z])+([0-9a-zA-Z])*@([a-zA-Z])+\\.([a-zA-Z])+")) {
+			flash.message = "Email invalido!"
+			redirect(controller: "contato", action:"index")
 		} else {
-			def mensagem = "\r\nde: " + params.email + "\r\n\r\n" + params.mensagem + "\r\nMensagem enviada a partir do Sistema de Gerenciamento do EJC da paróquia de São cristóvão!"
+			def mensagem = "\r\nde: " + params.email + "\r\n\r\n" + params.mensagem + "\r\n\r\nMensagem enviada a partir do Sistema de Gerenciamento do EJC da paróquia de São cristóvão!"
 			Usuario.list().each{ u -> 
 				if (u.j5Atual) {
 					senderService.enviaEmail(u.email, '[Sistema EJC] Mensagem', mensagem)
