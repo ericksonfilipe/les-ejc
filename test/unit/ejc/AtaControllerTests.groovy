6b38bc74393f56b41fbb7fed557c30eec70d2804
janderson jason <janderson.jason@gmail.com>
2011-06-29 21:08:48 -0300

    Organizando o uso de /ejc - falta testes funcionais

---
+++ test/unit/ejc/AtaControllerTests.groovy
@@ -0,0 +1,17 @@
+package ejc
+
+import grails.test.*
+
+class AtaControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
